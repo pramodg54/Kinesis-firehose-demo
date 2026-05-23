5d4e687710c42458389a700bf08a2038dbc6842b
pramodg54 <pramodg54@gmail.com>
2026-05-23 20:01:51 +0530

    Update app.py
    v4

--- app.py
+++ app.py
@@ -1,21 +1,55 @@
 import streamlit as st
-import pandas as pd
-import random 
-import time
-from datetime import datetime
-st.title("Kinesis Firehouse Streaming Demo")
-data = []
-placeholder = st.empty()
-for i in range(20):
-  new_data = {
-    "device_id" : random.randint(1000, 9999),
-    "temperature" : random.randint(20, 40),
-    "humidity" : random.randint(40, 90),
-    "time" : datetime.now().strftime("%H:%M:%S")
-  }
-data.append(new_data)
-df=pd.DataFrame(data)
-placeholder.dataframe(df)
-time.sleep(1)
 
-    
+import pandas as pd
+
+import random
+
+import time
+
+from datetime import datetime
+
+ 
+
+st.title("Kinesis Firehose Streaming Demo")
+
+ 
+
+data = []
+
+ 
+
+placeholder = st.empty()
+
+ 
+
+for i in range(20):
+
+ 
+
+    new_data = {
+
+        "device_id": random.randint(1000, 9999),
+
+        "temperature": random.randint(20, 40),
+
+        "humidity": random.randint(40, 90),
+
+        "time": datetime.now().strftime("%H:%M:%S")
+
+    }
+
+ 
+
+    data.append(new_data)
+
+ 
+
+    df = pd.DataFrame(data)
+
+ 
+
+    placeholder.dataframe(df)
+
+ 
+
+    time.sleep(1)
